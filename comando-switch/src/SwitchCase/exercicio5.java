078487b6ff440e933dc8a079df8b3618d4b522af
vinisebold <vinicius.sebold05@gmail.com>
2025-05-20 17:13:23 -0300

    feat: adiciona três exercicios de switch case

---
+++ comando-switch/src/SwitchCase/exercicio5.java
@@ -0,0 +1,40 @@
+package SwitchCase;
+
+import java.util.Scanner;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+
+        //Inicializa Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Entrada de dados
+        System.out.print("Informe o código da condição de pagamento: ");
+        int condicaoPagamento = sc.nextInt();
+        System.out.print("Informe o valor da etiqueta: ");
+        double valorEtiqueta = sc.nextDouble();
+
+        //Processamento e Output
+        switch (condicaoPagamento) {
+            case 1:
+                System.out.println("À vista em dinheiro ou cheque, recebe 10% de desconto");
+                System.out.println("Total da compra: R$" + valorEtiqueta * 0.9);
+                break;
+            case 2:
+                System.out.println("À vista no cartão de crédito, recebe 15% de desconto");
+                System.out.println("Total da compra: R$" + valorEtiqueta * 0.85);
+                break;
+            case 3:
+                System.out.println("Em duas vezes, preço normal de etiqueta sem juros");
+                System.out.println("Total da compra: 2 parcelas de R$" + valorEtiqueta / 2);
+                break;
+            case 4:
+                System.out.println("Em três vezes, preço normal de etiqueta mais juros de 10%");
+                System.out.println("Total da compra: 3 parcelas de R$" + (valorEtiqueta * 1.1) / 3);
+                break;
+            default:
+                System.out.println("Valor fora dos padrões! Programa cancelado!");
+                System.exit(0);
+        }
+    }
+}
---
+++ comando-switch/src/SwitchCase/exercicio6.java
@@ -0,0 +1,43 @@
+package SwitchCase;
+
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+        // Inicializa Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Declaração de Variáveis e Entrada de Dados
+        System.out.print("Informe a nota 1");
+        double notaInf1 = sc.nextDouble();
+        System.out.print("Informe a nota 2");
+        double notaInf2 = sc.nextDouble();
+        System.out.print("Informe a nota 3");
+        double notaInf3 = sc.nextDouble();
+
+        // Calculo Média
+        double mediaCalculada = (notaInf1 + notaInf2 + notaInf3) / 3;
+
+        // Processamento
+        var conceitoAluno = 'A';
+        if (mediaCalculada >= 8) {
+            conceitoAluno = 'A';
+        } else if (mediaCalculada >= 6 && mediaCalculada <= 7.99) {
+            conceitoAluno = 'B';
+        } else {
+            conceitoAluno = 'C';
+        }
+        switch (conceitoAluno) {
+            case 'A':
+                System.out.println("Aprovado");
+                break;
+            case 'B':
+                System.out.println("Em exame");
+                break;
+            case 'C':
+                System.out.println("Reprovado");
+                break;
+
+        }
+    }
+}
